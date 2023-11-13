814723e4ab1e06577c7d9af439945acbee88cac8
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-11-13 19:24:18 +0530

    minor data tweak

--- src/components/ApplicationCard.tsx
+++ src/components/ApplicationCard.tsx
@@ -26,20 +26,20 @@ export default function ApplicationCard(props: { application: any }) {
         <div className="flex justify-between gap-x-4 py-3">
           <dt className="text-gray-500">Last invoice</dt>
           <dd className="text-gray-700">
-            <time dateTime={application.lastInvoice.dateTime}>{application.lastInvoice.date}</time>
+            <time dateTime={application.dateTime}>{application.date}</time>
           </dd>
         </div>
         <div className="flex justify-between gap-x-4 py-3">
           <dt className="text-gray-500">Amount</dt>
           <dd className="flex items-start gap-x-2">
-            <div className="font-medium text-gray-900">{application.lastInvoice.amount}</div>
+            <div className="font-medium text-gray-900">{application.amount}</div>
             <div
               className={classNames(
-                // statuses[application.lastInvoice.status],
+                // statuses[application.status],
                 'rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset'
               )}
             >
-              {application.lastInvoice.status}
+              {application.status}
             </div>
           </dd>
         </div>
