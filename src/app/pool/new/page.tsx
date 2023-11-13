1b145990a750078bf64455eee7fae61f5ae14a02
0xKurt <Kurt@Merbeth.io>
2023-11-13 13:17:44 +0100

    test deploy

--- src/app/pool/new/page.tsx
+++ src/app/pool/new/page.tsx
@@ -1,17 +1,35 @@
+"use client";
 import Container from "@/components/Container";
-import MicroGrantsStrategy from "../../../allo-v2-sdk/dist/strategies/MicroGrantsStrategy/MicroGrantsStrategy"
+
+import { MicroGrantsStrategy } from "../../../../allo-v2-sdk/dist/strategies/MicroGrantsStrategy/MicroGrantsStrategy";
+import { sendTransaction } from "@wagmi/core";
+
 const NewPool = () => {
-
   const strategy = new MicroGrantsStrategy({
-    chain:,
-    rpc:,
-    address:,
-    poolId:,
+    chain: 5,
   });
-  return;
-  <Container>
 
-  </Container>;
+  const deploy = async () => {
+    const deployParams = strategy.getDeployParams();
+    const { hash } = await sendTransaction({
+      to: "0x0000000000000000000000000000000000000000",
+      data: deployParams.bytecode,
+    });
+
+    console.log(hash);
+  };
+
+  const deployData = strategy.getDeployParams();
+
+  // console.log(deployData);
+
+  return (
+    <Container>
+      <div>
+        hello <button onClick={() => deploy()}>deploy</button>
+      </div>
+    </Container>
+  );
 };
 
 export default NewPool;
