1fe2063fe49482e9f06a09d82bba82e590b3c5ac
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-11-16 16:53:35 +0530

    chore: add queries

---
+++ src/utils/query.ts
@@ -0,0 +1,47 @@
+import { gql } from "graphql-request";
+
+
+export const graphqlEndpoint = process.env.GRAPHQL_URL || "";
+
+/** Returns all the MicroGrants pools */
+export const getMicroGrantsQuery = gql`
+  {
+    microGrants(orderBy: BLOCK_TIMESTAMP_DESC) {
+      poolId
+      strategy
+      allocationStartTime
+      allocationEndTime
+      approvalThreshold
+      maxRequestedAmount
+      pool {
+          tokenMetadata
+          token
+          amount
+          metadataPointer
+          profile {
+            profileId
+            name
+          }
+      }
+    }
+  }
+`;
+
+/** Returns the recipient information for micro grant strategy */
+export const getMicroGrantsRecipientsQuery = gql`
+  query getMicroGrantsRecipientsQuery($chainId: String!, $strategyId: String!) {
+    microGrant(chainId: $chainId, strategy: $strategyId) {
+      microGrantRecipients {
+        recipientAddress
+        recipientAddress
+        requestedAmount
+        metadataPointer
+        blockTimestamp
+        isUsingRegistryAnchor
+        status
+      }
+    }
+  }
+`;
+
+// TODO: add query for getting allocations to track approvals/rejecte
