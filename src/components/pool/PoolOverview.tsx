4f820437abb0505ee4320484835de8d0798a90b7
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-11-21 20:36:48 +0530

    prettyify (#54)
    * prettyify
    
    * fix

--- src/components/pool/PoolOverview.tsx
+++ src/components/pool/PoolOverview.tsx
@@ -140,7 +140,7 @@ export default function PoolOverview(props: {
         )}
         {currentTab == "Reviews" && (
           <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-10">
-            <div className="lg:col-span-2 lg:pr-8 px-6">
+            <div className="lg:col-span-2 lg:pr-8">
               <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
                 Application Reviews
               </h1>
