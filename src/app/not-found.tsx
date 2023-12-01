c192a96c00bb938ebbe764ae369a24e13da3a238
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-12-01 11:35:59 +0530

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
         <h1 className="text-9xl font-bold text-gray-900">404</h1>
         <h2 className="text-2xl font-semibold text-gray-900">Page not found</h2>
         <p className="text-xl text-gray-600">
-          Sorry, we couldn't find the page you're looking for.
+          { "Sorry, we couldn't find the page you're looking for." }
         </p>
       </div>
     </div>
