98e00f42f389f08b11bfb6b72e559ca1eaebd89c
Aditya Anand M C <aditya.anandmc@gmail.com>
2023-12-20 01:11:05 +0530

    remove dropdown

--- src/components/shared/Icons.tsx
+++ src/components/shared/Icons.tsx
@@ -27,8 +27,8 @@ export function NewApplicationIcon() {
 export function NewPoolIcon() {
   return (
     <svg
-      width="48"
-      height="48"
+      width="30"
+      height="30"
       viewBox="0 0 48 48"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
