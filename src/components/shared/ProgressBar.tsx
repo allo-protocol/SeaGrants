89c1bd0c82a1a8737a76e9e982aa68c5dafd8daf
0xKurt <Kurt@Merbeth.io>
2023-12-19 23:32:24 +0100

    fix progress bar

--- src/components/shared/ProgressBar.tsx
+++ src/components/shared/ProgressBar.tsx
@@ -5,12 +5,14 @@ export default function ProgressBar(props: {current: number, total: number}) {
         <div className="overflow-hidden rounded-full bg-gray-200">
           <div
             className="h-2 rounded-full bg-indigo-600"
-            style={{ width: `${(props.current * 100 ) / props.total}%` }}
+            style={{ width: `${(props.current * 100) / props.total}%` }}
           />
         </div>
         <div className="mt-2 hidden grid-cols-3 text-sm font-medium text-gray-600 sm:grid">
           <div className="text-indigo-600">0</div>
-          <div className="text-center text-indigo-600">{props.current}</div>
+          <div className="text-center text-indigo-600">
+            {props.current < props.total ? props.current : ""}
+          </div>
           <div className="text-right">{props.total}</div>
         </div>
       </div>
